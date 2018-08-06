65647f4aec11c639eefe83db7a32e9f17ddc9b79
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-08-05 23:23:41 -0500

    :clipboard: functionality created, app registering users, and validations

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,5 +1,5 @@
 const User = require('../models/User')
-
+const bcrypt = require('bcrypt')
 exports.get_users = (req, res, next) => {
     User.find()
         .select('_id username email')
@@ -25,4 +25,72 @@ exports.get_users = (req, res, next) => {
                 error: err
             })
         })
+}
+exports.save_user = (req, res, next) => {
+    console.log(req.body)
+    User.find({
+        email: req.body.email
+    })
+    .exec()
+    .then(result => {
+        if (result.length > 0) {
+            return res.status(409).json({
+                message: 'Email already exists.'
+            })
+        }
+        if (req.body.password) {
+            bcrypt.hash(req.body.password,10, (err, hash) => {
+                if (err) {
+                    return res.status(200).json({
+                        error: err
+                    })
+                }
+                if (req.body.email != null
+                    && req.body.name != null
+                    && req.body.username != null) {
+                    const user = new User()
+                    user.name = req.body.name
+                    user.email = req.body.email
+                    user.username = req.body.username
+                    user.image = 'null'
+                    user.role = 'ROLE_USER'
+                    user.password = hash
+                    user.save((err, userStored) => {
+                        if (err) {
+                            res.status(500).json({
+                                error :err
+                            })
+                        }
+                        if (!userStored) {
+                            res.status(401).json({
+                                message: 'Error saving user'
+                            })
+                        }
+                        else {
+                            res.status(200).json({
+                                message: 'User created successfully',
+                                user : userStored
+                            })
+                        }
+                    } )
+                    
+                }
+                else {
+                    res.status(200).json({
+                        message: 'All fields are required'
+                    })
+                }
+            })
+        }
+        else{
+            res.status(200).json({
+                error: 'Password is required'
+            })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error : err
+        })
+    })
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const api = express.Router()
 const userController = require('../controllers/users')
 
-api.get('/users', userController.get_users)
+api.get('/', userController.get_users)
+api.post('/register', userController.save_user)
 
 module.exports = api
