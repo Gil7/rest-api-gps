4cad281c8a021756c64c9675e043432e21d437c1
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-08-05 22:13:07 -0500

    :clipboard: User model created

---
+++ api/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+    name: String,
+    username: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: String,
+    image: String
+})
+
+module.exports = new mongoose.model('User', UserSchema)
